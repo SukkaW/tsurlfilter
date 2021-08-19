93e4fc9f46290c2da12b53790f948d593bec0e0b
mizzick <ds.kolyshev@gmail.com>
2021-08-19 16:07:54 +0300

    feature:AG-9161 performance trie tests

--- test/utils/trie.test.ts
+++ test/utils/trie.test.ts
@@ -8,11 +8,22 @@ describe('Trie tests', () => {
 
         trie.add('test-1', 0);
         expect(trie.traverse('test-1', 0)).toHaveLength(1);
+        expect(trie.traverse('test-1', 0)).toContain(0);
         expect(trie.traverse('another', 0)).toHaveLength(0);
 
         trie.add('test-2', 1);
         expect(trie.traverse('test-1', 0)).toHaveLength(1);
+        expect(trie.traverse('test-1', 0)).toContain(0);
         expect(trie.traverse('test-2', 0)).toHaveLength(1);
+        expect(trie.traverse('test-2', 0)).toContain(1);
+        expect(trie.traverse('another', 0)).toHaveLength(0);
+
+        trie.add('test-1-1', 11);
+        const numbers = trie.traverse('test-1-1', 0);
+        expect(numbers).toHaveLength(2);
+        expect(numbers).toContain(0);
+        expect(numbers).toContain(11);
+
         expect(trie.traverse('another', 0)).toHaveLength(0);
     });
 });
