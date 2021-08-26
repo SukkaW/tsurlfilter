97e60becffbb7c762d4cb8fee62da19758a0ed98
mizzick <ds.kolyshev@gmail.com>
2021-08-26 17:05:29 +0300

    feature:AG-9161 Engine class sample

--- examples/manifest-v2/extension/src/background/application.js
+++ examples/manifest-v2/extension/src/background/application.js
@@ -284,12 +284,42 @@ export class Application {
     }
 
     /**
+     * Frame rules cache
+     * @type {Map<any, any>}
+     */
+    frameRules = new Map();
+
+    /**
+     * Records frame
+     *
+     * @param request
+     */
+    recordFrame(request) {
+        const frameUrl = request.sourceUrl;
+        const rule = this.engine.matchFrame(frameUrl);
+        this.frameRules.set(frameUrl, rule);
+    }
+
+    /**
+     * Returns frame rule if found
+     *
+     * @param request
+     */
+    getFrameRule(request) {
+        return this.frameRules.get(request.sourceUrl);
+    }
+
+    /**
      * Gets matching result
      *
      * @return {MatchingResult}
      */
     getMatchingResult(request) {
-        const frameRule = this.engine.matchFrame(request.sourceUrl);
+        if (request.requestType === TSUrlFilter.RequestType.Document) {
+            this.recordFrame(request);
+        }
+
+        const frameRule = this.getFrameRule(request);
         return this.engine.matchRequest(request, frameRule);
     }
 
