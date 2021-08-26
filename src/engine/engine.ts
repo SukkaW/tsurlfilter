b7e752f054dc83220e3431c7dd02712ad5c98c76
mizzick <ds.kolyshev@gmail.com>
2021-08-26 18:02:32 +0300

    feature:AG-9161 performance REQUEST_CACHE_SIZE

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -21,7 +21,7 @@ export class Engine {
      * Used as both source rules and others limit.
      * The value is based on benchmark runs.
      */
-    private static REQUEST_CACHE_SIZE = 5000;
+    private static REQUEST_CACHE_SIZE = 500;
 
     /**
      * Basic filtering rules engine
