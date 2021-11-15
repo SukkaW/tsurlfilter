541615c29512e9d560a55dda37bd03c9e650b677
mizzick <ds.kolyshev@gmail.com>
2021-11-15 13:45:17 +0200

    feature:AG-10476 tswebextension services misc

--- packages/tswebextension/rollup.config.ts
+++ packages/tswebextension/rollup.config.ts
@@ -1,46 +1,12 @@
 import typescript from '@rollup/plugin-typescript';
 import cleanup from 'rollup-plugin-cleanup';
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
 
 const DEFAULT_OUTPUT_PATH = 'dist';
 
 const OUTPUT_PATH = process.env.PACKAGE_OUTPUT_PATH ? `${process.env.PACKAGE_OUTPUT_PATH}/dist` : DEFAULT_OUTPUT_PATH;
 
-const contentScriptLibraryName = 'TSWebExtensionContentScript';
-
-const contentScriptConfig = {
-    input: 'src/content-script/index.ts',
-    output: [
-        {
-            file: `${OUTPUT_PATH}/${contentScriptLibraryName}.js`,
-            format: 'esm',
-            sourcemap: false,
-        },
-        {
-            file: `${OUTPUT_PATH}/${contentScriptLibraryName}.umd.js`,
-            name: contentScriptLibraryName,
-            format: 'umd',
-            sourcemap: false,
-        },
-    ],
-    watch: {
-        include: 'src/**',
-    },
-    plugins: [
-        typescript(),
-        commonjs({
-            sourceMap: false,
-        }),
-        resolve(),
-        cleanup({
-            comments: ['srcmaps'],
-        }),
-    ],
-};
 
 export default {
-    contentScriptConfig,
     input: [
         'src/index.ts',
     ],
