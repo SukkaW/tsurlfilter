dde859464999a74d206774e832e136f780de7df0
mizzick <ds.kolyshev@gmail.com>
2021-11-18 11:56:25 +0200

    feature:AG-10476 tswebextension services misc

--- packages/tswebextension/src/content-script/index.js
+++
@@ -1,9 +0,0 @@
-import ExtendedCss from 'extended-css';
-import StealthHelper from './stealth-helper';
-import CookieController from './cookie-controller';
-import CssHitsCounter from './css-hits-counter';
-
-/**
- * This module exports libraries used in the extension via content scripts
- */
-export default { ExtendedCss, StealthHelper, CookieController, CssHitsCounter };
