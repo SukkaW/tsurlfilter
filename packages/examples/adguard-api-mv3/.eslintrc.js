92e5f82ab26644be09f97ebb62963add92861d60
Dmitriy Seregin <d.seregin@adguard.com>
2022-12-01 14:16:45 +0400

    removed prettier

--- packages/examples/adguard-api-mv3/.eslintrc.js
+++ packages/examples/adguard-api-mv3/.eslintrc.js
@@ -10,16 +10,66 @@ module.exports = {
     env: {
         browser: true,
     },
+    plugins: [
+        'import',
+        'import-newlines',
+    ],
     extends: [
         'airbnb-base',
         'airbnb-typescript/base',
         'plugin:jsdoc/recommended',
-        'plugin:prettier/recommended'
+        'plugin:@typescript-eslint/recommended',
     ],
     rules: {
-        'import/prefer-default-export': 0,
-        'jsdoc/require-param-type': 0,
-        'jsdoc/require-returns-type': 0,
+        'max-len': ['error', { code: 120 }],
 
+        'import-newlines/enforce': ['error', 3, 120],
+        'import/no-extraneous-dependencies': ['error', { 'devDependencies': true }],
+        'import/prefer-default-export': 'off',
+        'import/order': [
+            'error',
+            {
+                groups: [
+                    "builtin",
+                    "external",
+                    "internal",
+                    "parent",
+                    "index",
+                ],
+                'newlines-between': 'always',
+                warnOnUnassignedImports: false,
+            },
+        ],
+
+        '@typescript-eslint/indent': ['error', 4],
+        '@typescript-eslint/explicit-function-return-type': 'error',
+
+        'jsdoc/require-param-type': 'off',
+        'jsdoc/require-returns-type': 'off',
+        'jsdoc/require-throws': 'error',
+        'jsdoc/require-file-overview': 'error',
+        'jsdoc/require-description-complete-sentence': ['warn'],
+        'jsdoc/require-jsdoc': [
+            'warn',
+            {
+                contexts: [
+                    'ClassDeclaration',
+                    'ClassProperty',
+                    'FunctionDeclaration',
+                    'MethodDefinition'
+                ]
+            }
+        ],
+        'jsdoc/require-description': [
+            'warn',
+            {
+                contexts: [
+                    'ClassDeclaration',
+                    'ClassProperty',
+                    'FunctionDeclaration',
+                    'MethodDefinition'
+                ]
+            }
+        ],
     },
 };
