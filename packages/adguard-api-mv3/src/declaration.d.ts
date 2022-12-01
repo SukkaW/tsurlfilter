7a1e0fa559b5f4f1e30b75d27383a17ffe6519a7
Dmitriy Seregin <d.seregin@adguard.com>
2022-12-01 14:16:56 +0400

    fix

--- packages/adguard-api-mv3/src/declaration.d.ts
+++ packages/adguard-api-mv3/src/declaration.d.ts
@@ -1,5 +1,6 @@
+/* eslint-disable jsdoc/require-file-overview */
 // TODO: Remove when chrome will update @types (last checked in @types/chrome v0.0.193:)
-declare module chrome.declarativeNetRequest {
+declare namespace chrome.declarativeNetRequest {
     // The maximum number of static Rulesets an extension can enable at any one time.
     export const MAX_NUMBER_OF_ENABLED_STATIC_RULESETS: number;
 }
