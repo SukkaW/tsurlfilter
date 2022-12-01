e184f9b752293862e833d9a5a2db9db191f338d0
Dmitriy Seregin <d.seregin@adguard.com>
2022-12-02 01:09:09 +0400

    fixed docs for main.ts

--- packages/adguard-api-mv3/src/background/main.ts
+++ packages/adguard-api-mv3/src/background/main.ts
@@ -16,9 +16,10 @@
  * along with Adguard API. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { EventChannel } from '@adguard/tswebextension';
 import {
     TsWebExtension,
-    Configuration,
+    Configuration as TsWebExtensionConfiguration,
     ConfigurationResult,
     RULE_SET_NAME_PREFIX,
     TooManyRulesError,
@@ -51,39 +52,58 @@ export type RulesStatus = {
 
 export const WEB_RESOURCES_PATH = '/adguard/resources';
 
-const { MAX_NUMBER_OF_REGEX_RULES, MAX_NUMBER_OF_DYNAMIC_AND_SESSION_RULES } = chrome.declarativeNetRequest;
+const {
+    MAX_NUMBER_OF_REGEX_RULES,
+    MAX_NUMBER_OF_DYNAMIC_AND_SESSION_RULES,
+} = chrome.declarativeNetRequest;
 
 interface AdguardApiInterface {
-    // onAssistantCreateRule: EventChannel<string>;
-    // onRequestBlocked: RequestBlockingLogger;
-    start(configuration: Configuration): Promise<Configuration>;
+    onAssistantCreateRule: EventChannel<string>;
+    start(configuration: APIConfiguration): Promise<void>;
     stop(): Promise<void>;
-    configure(configuration: Configuration): Promise<Configuration>;
+    configure(configuration: APIConfiguration): Promise<void>;
     openAssistant(tabId: number): Promise<void>;
     closeAssistant(tabId: number): Promise<void>;
     getRulesCount(): number;
 }
 
 /**
- *
+ * AdGuard API is filtering library, provided following features:
+ * - request and content filtering, using {@link TsWebExtension},
+ * - content blocking via AdGuard Assistant UI, provided by {@link TsWebExtension}.
  */
-export default class AdguardApi {
+export default class AdguardApi implements AdguardApiInterface {
+    // Engine instance
     private tsWebExtension: TsWebExtension;
 
+    // Stores result of last call configuration to get status of current enabled
+    // rules
     private configurationResult: ConfigurationResult | undefined;
 
+    // Waiting for start engine to prevent race conditions
     private waitForStart: Promise<void> | undefined;
 
+    // Is declarative filtering log enabled or not
     public filteringLogEnabled = false;
 
+    // Stores handler for "inner" messages
     private messageHandler: MessagesHandlerType;
 
     /**
+     * {@link TsWebExtension} {@link EventChannel},
+     * which fires event on assistant rule creation.
+     */
+    public onAssistantCreateRule: EventChannel<string>;
+
+    /**
+     * Creates new AdGuard API class.
      *
-     * @param webAccessibleResourcesPath
+     * @param webAccessibleResourcesPath - Path to the web accessible resources.
      */
     constructor(webAccessibleResourcesPath = WEB_RESOURCES_PATH) {
         this.tsWebExtension = new TsWebExtension(webAccessibleResourcesPath);
+
+        this.onAssistantCreateRule = this.tsWebExtension.onAssistantCreateRule;
 
         this.messageHandler = this.tsWebExtension.getMessageHandler();
 
@@ -91,7 +111,9 @@ export default class AdguardApi {
     }
 
     /**
+     * Returns counters of current enabled static rule sets.
      *
+     * @returns Counters of current enabled static rule sets.
      */
     public get ruleSetsCounters(): RuleSetCounters[] {
         return (
@@ -103,14 +125,13 @@ export default class AdguardApi {
         );
     }
 
-    // FIXME: Add reinitialization with read last provided config values from memory
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     private messageHandlerWrapper = (message: any, sender: any, sendResponse: (response?: any) => void): boolean => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         (async (): Promise<any> => {
             if (this.waitForStart) {
                 await this.waitForStart;
             }
-
-            // logger.debug("[messageHandlerWrapper]: handle message", message);
 
             // TODO: use MESSAGE_HANDLER_NAME
             if (message.handlerName === 'tsWebExtension') {
@@ -120,7 +141,7 @@ export default class AdguardApi {
             return null;
         })()
             .then(sendResponse)
-            .catch((e: any) => {
+            .catch((e) => {
                 sendResponse({ error: { message: e.message } });
             });
 
@@ -128,8 +149,9 @@ export default class AdguardApi {
     };
 
     /**
+     * Starts engine.
      *
-     * @param configuration
+     * @param configuration {@link APIConfiguration}.
      */
     async start(configuration: APIConfiguration): Promise<void> {
         const start = async (): Promise<void> => {
@@ -145,16 +167,18 @@ export default class AdguardApi {
     }
 
     /**
-     *
+     * Stops engine.
      */
     async stop(): Promise<void> {
         await this.tsWebExtension.stop();
     }
 
     /**
+     * Modifies AdGuard {@link APIConfiguration}.
      *
-     * @param configuration
-     * @param skipCheck
+     * @param configuration {@link APIConfiguration}.
+     * @param skipCheck Whether it is necessary to check whether the limit
+     * is exceeded.
      */
     async configure(configuration: APIConfiguration, skipCheck?: boolean): Promise<void> {
         const config = await this.getConfiguration(configuration);
@@ -168,9 +192,11 @@ export default class AdguardApi {
     }
 
     /**
+     * Returns information about dynamic rules from current configuration.
      *
-     * @param root0
-     * @param root0.dynamicRules
+     * @param root0 {@link ConfigurationResult}.
+     * @param root0.dynamicRules {@link ConversionResult}.
+     * @returns Object {@link DynamicRulesStatus} with counters of dynamic rules.
      */
     static async getDynamicRulesInfo({ dynamicRules }: ConfigurationResult): Promise<DynamicRulesStatus | null> {
         const {
@@ -207,16 +233,11 @@ export default class AdguardApi {
     }
 
     /**
-     * .
-     * TODO: Check this
+     * TODO: Check this.
      * If changed - save new values to store for show warning to user
      * and save list of last used filters.
      *
-     * @param configuration
-     */
-    /**
-     *
-     * @param configuration
+     * @param configuration {@link APIConfiguration}.
      */
     private async checkFiltersLimitsChange(configuration: APIConfiguration): Promise<void> {
         const wasEnabledIds = configuration.filters.sort((a: number, b: number) => a - b);
@@ -239,8 +260,6 @@ export default class AdguardApi {
         };
 
         const brokenState = isDifferent();
-
-        // await browserActions.setIconBroken(brokenState);
 
         // FIXME: If state has been broken - return new applied configuration
         if (brokenState) {
@@ -265,7 +284,13 @@ export default class AdguardApi {
         return ids || [];
     };
 
-    private getConfiguration = async (configuration: APIConfiguration): Promise<Configuration> => {
+    /**
+     * Returns configuration object.
+     *
+     * @param configuration {@link APIConfiguration}.
+     * @returns TsWebExtension configuration {@link TsWebExtensionConfiguration}.
+     */
+    private getConfiguration = async (configuration: APIConfiguration): Promise<TsWebExtensionConfiguration> => {
         const { installType } = await chrome.management.getSelf();
         const isUnpacked = installType === 'development';
 
@@ -276,7 +301,6 @@ export default class AdguardApi {
                 collectStats: true,
                 stealthModeEnabled: false,
                 filteringEnabled: false,
-                // TODO: check fields needed in the mv3
                 stealth: {
                     blockChromeClientData: false,
                     hideReferrer: false,
@@ -303,15 +327,47 @@ export default class AdguardApi {
 
     /**
      * Returns tswebextension messages handler.
+     *
+     * @returns Messages handler for "inner" tswebextension messages.
      */
     getMessageHandler(): MessagesHandlerType {
         return this.tsWebExtension.getMessageHandler();
     }
 
     /**
-     * Creates new adguardApi instance
+     * Opens the AdGuard assistant in the specified tab.
      *
-     * @returns AdguardApi instance
+     * @param tabId The ID of the tab where is needed to open
+     * the AdGuard assistant.
+     */
+    public async openAssistant(tabId: number): Promise<void> {
+        await this.tsWebExtension.openAssistant(tabId);
+    }
+
+    /**
+     * Closes the AdGuard assistant in the specified tab.
+     *
+     * @param tabId The ID of the tab where is needed to close
+     * the AdGuard assistant.
+     */
+    public async closeAssistant(tabId: number): Promise<void> {
+        await this.tsWebExtension.closeAssistant(tabId);
+    }
+
+    /**
+     * Gets current loaded rules in the filtering engine
+     * (except declarative rules).
+     *
+     * @returns Number of loaded rules in the filtering engine.
+     */
+    public getRulesCount(): number {
+        return this.tsWebExtension.getRulesCount();
+    }
+
+    /**
+     * Creates new adguardApi instance.
+     *
+     * @returns AdguardApi instance.
      */
     public static create(): AdguardApi {
         return new AdguardApi();
