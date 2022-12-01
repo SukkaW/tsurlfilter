2f8479637e97e1cf4dee97579e4a3a5abe461271
Dmitriy Seregin <d.seregin@adguard.com>
2022-12-02 01:11:54 +0400

    added counter and assistant to the example

--- packages/adguard-api-mv3/dist/types/background/main.d.ts
+++ packages/adguard-api-mv3/dist/types/background/main.d.ts
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Adguard API. If not, see <http://www.gnu.org/licenses/>.
  */
+import { EventChannel } from '@adguard/tswebextension';
 import { ConfigurationResult, MessagesHandlerType } from '@adguard/tswebextension/mv3';
 import { APIConfiguration } from './schemas';
 export declare type RuleSetCounters = {
@@ -34,70 +35,119 @@ export declare type RulesStatus = {
     excludedRulesIds: number[];
 };
 export declare const WEB_RESOURCES_PATH = "/adguard/resources";
+interface AdguardApiInterface {
+    onAssistantCreateRule: EventChannel<string>;
+    start(configuration: APIConfiguration): Promise<void>;
+    stop(): Promise<void>;
+    configure(configuration: APIConfiguration): Promise<void>;
+    openAssistant(tabId: number): Promise<void>;
+    closeAssistant(tabId: number): Promise<void>;
+    getRulesCount(): number;
+}
 /**
- *
+ * AdGuard API is filtering library, provided following features:
+ * - request and content filtering, using {@link TsWebExtension},
+ * - content blocking via AdGuard Assistant UI, provided by {@link TsWebExtension}.
  */
-export default class AdguardApi {
+export default class AdguardApi implements AdguardApiInterface {
     private tsWebExtension;
     private configurationResult;
     private waitForStart;
     filteringLogEnabled: boolean;
     private messageHandler;
     /**
+     * {@link TsWebExtension} {@link EventChannel},
+     * which fires event on assistant rule creation.
+     */
+    onAssistantCreateRule: EventChannel<string>;
+    /**
+     * Creates new AdGuard API class.
      *
-     * @param webAccessibleResourcesPath
+     * @param webAccessibleResourcesPath - Path to the web accessible resources.
      */
     constructor(webAccessibleResourcesPath?: string);
     /**
+     * Returns counters of current enabled static rule sets.
      *
+     * @returns Counters of current enabled static rule sets.
      */
     get ruleSetsCounters(): RuleSetCounters[];
     private messageHandlerWrapper;
     /**
+     * Starts engine.
      *
-     * @param configuration
+     * @param configuration {@link APIConfiguration}.
      */
     start(configuration: APIConfiguration): Promise<void>;
     /**
-     *
+     * Stops engine.
      */
     stop(): Promise<void>;
     /**
+     * Modifies AdGuard {@link APIConfiguration}.
      *
-     * @param configuration
-     * @param skipCheck
+     * @param configuration {@link APIConfiguration}.
+     * @param skipCheck Whether it is necessary to check whether the limit
+     * is exceeded.
      */
     configure(configuration: APIConfiguration, skipCheck?: boolean): Promise<void>;
     /**
+     * Returns information about dynamic rules from current configuration.
      *
-     * @param root0
-     * @param root0.dynamicRules
+     * @param root0 {@link ConfigurationResult}.
+     * @param root0.dynamicRules {@link ConversionResult}.
+     * @returns Object {@link DynamicRulesStatus} with counters of dynamic rules.
      */
     static getDynamicRulesInfo({ dynamicRules }: ConfigurationResult): Promise<DynamicRulesStatus | null>;
     /**
-     * .
-     * TODO: Check this
+     * TODO: Check this.
      * If changed - save new values to store for show warning to user
      * and save list of last used filters.
      *
-     * @param configuration
-     */
-    /**
-     *
-     * @param configuration
+     * @param configuration {@link APIConfiguration}.
      */
     private checkFiltersLimitsChange;
     private static setFiltersChangedList;
     private static getFiltersChangedList;
+    /**
+     * Returns configuration object.
+     *
+     * @param configuration {@link APIConfiguration}.
+     * @returns TsWebExtension configuration {@link TsWebExtensionConfiguration}.
+     */
     private getConfiguration;
     /**
      * Returns tswebextension messages handler.
+     *
+     * @returns Messages handler for "inner" tswebextension messages.
      */
     getMessageHandler(): MessagesHandlerType;
     /**
-     * Creates new adguardApi instance
+     * Opens the AdGuard assistant in the specified tab.
      *
-     * @returns AdguardApi instance
+     * @param tabId The ID of the tab where is needed to open
+     * the AdGuard assistant.
+     */
+    openAssistant(tabId: number): Promise<void>;
+    /**
+     * Closes the AdGuard assistant in the specified tab.
+     *
+     * @param tabId The ID of the tab where is needed to close
+     * the AdGuard assistant.
+     */
+    closeAssistant(tabId: number): Promise<void>;
+    /**
+     * Gets current loaded rules in the filtering engine
+     * (except declarative rules).
+     *
+     * @returns Number of loaded rules in the filtering engine.
+     */
+    getRulesCount(): number;
+    /**
+     * Creates new adguardApi instance.
+     *
+     * @returns AdguardApi instance.
      */
     static create(): AdguardApi;
 }
+export {};
--- packages/examples/adguard-api-mv3/extension/src/index.ts
+++ packages/examples/adguard-api-mv3/extension/src/index.ts
@@ -1,29 +1,59 @@
+/* eslint-disable jsdoc/require-file-overview */
 /* eslint-disable no-console */
-import { AdguardApi, APIConfiguration } from "@adguard/api";
+import { AdguardApi, APIConfiguration } from '@adguard/api';
 
-// FIXME: Add assistant
-// FIXME: Add counter
 (async (): Promise<void> => {
     const configuration: APIConfiguration = {
         filters: [2],
-        rules: ["example.org##h1"],
+        rules: ['example.org##h1'],
     };
 
     const adguardApi = AdguardApi.create();
 
+    // console log current rules count, loaded in engine
+    const logTotalCount = (): void => {
+        console.log('Total rules count:', adguardApi.getRulesCount());
+    };
+
     await adguardApi.start(configuration);
 
-    console.log("Finished Adguard API initialization.");
+    console.log('Finished Adguard API initialization.');
+    logTotalCount();
 
-    configuration.rules!.push("||google.com^$document");
+    configuration.rules!.push('||google.com^$document');
 
     await adguardApi.configure(configuration);
 
-    console.log("Finished Adguard API re-configuration");
+    console.log('Finished Adguard API re-configuration');
+    logTotalCount();
+
+    // update config on assistant rule apply
+    adguardApi.onAssistantCreateRule.subscribe(async (rule) => {
+        console.log(`Rule ${rule} was created by Adguard Assistant`);
+        configuration.rules!.push(rule);
+        await adguardApi.configure(configuration);
+        console.log('Finished Adguard API re-configuration');
+        logTotalCount();
+    });
+
+    chrome.runtime.onMessage.addListener(async (message) => {
+        switch (message.type) {
+            case 'OPEN_ASSISTANT': {
+                chrome.tabs.query({ active: true }, async (res) => {
+                    if (res[0]?.id) {
+                        await adguardApi.openAssistant(res[0].id);
+                    }
+                });
+                break;
+            }
+            default:
+            // do nothing
+        }
+    });
 
     // Disable Adguard in 1 minute
     setTimeout(async () => {
         await adguardApi.stop();
-        console.log("Adguard API has been disabled.");
+        console.log('Adguard API has been disabled.');
     }, 60 * 1000);
 })();
