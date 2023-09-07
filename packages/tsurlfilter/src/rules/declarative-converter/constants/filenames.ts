ab5c9afaf1bc04c57cdb1b0555b5c9a294ec21e3
Dmitriy Seregin <d.seregin@adguard.com>
2023-09-07 17:57:33 +0400

    fix

--- packages/tsurlfilter/src/rules/declarative-converter/constants/filenames.ts
+++ packages/tsurlfilter/src/rules/declarative-converter/constants/filenames.ts
@@ -6,4 +6,6 @@
 export const REGEXP_RULES_COUNT_FILENAME = 'regexp-rules-count';
 export const RULES_COUNT_FILENAME = 'rules-count';
 export const SOURCE_MAP_FILENAME_JSON = 'source-map.json';
+export const HASH_MAP_FILENAME_JSON = 'hashes.json';
+export const BAD_FILTER_RULES_FILENAME = 'bad-filter-rules.json';
 export const FILTER_LIST_IDS_FILENAME_JSON = 'filter-list-ids.json';
